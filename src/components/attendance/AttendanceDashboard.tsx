f8e1f743ed3492bd23ef9a26d637a4cbf14d4790
unknown <moriya_nishizawa@mforce.co.jp>
2025-06-11 03:41:29 +0900

    コンポーネント配置修正

--- src/components/attendance/AttendanceDashboard.tsx
+++ src/components/attendance/AttendanceDashboard.tsx
@@ -52,7 +52,6 @@ export default function AttendanceDashboard() {
             isLoading={summary.isLoading}
           />
         </Grid>
-
         <Grid item size={{ xs: 12, sm: 6, lg: 6 }}>
           <LineChart/>
         </Grid>
